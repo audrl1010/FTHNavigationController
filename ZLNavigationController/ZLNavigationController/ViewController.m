89f74f00f2089bcdee8635fd143ff7f3c5a1e123
PatrickChow <zdxf@qq.com>
2016-07-07 17:59:43 +0800

    demo

--- ZLNavigationController/ZLNavigationController/ViewController.m
+++ ZLNavigationController/ZLNavigationController/ViewController.m
@@ -24,6 +24,18 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     {
+        UIScrollView * scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+        
+        for (int i = 0; i < 10; i ++) {
+            UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, CGRectGetHeight(self.view.bounds)/5.0 * i, CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds)/5.0)];
+            view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1];
+            [scrollView addSubview:view];
+        }
+        scrollView.contentSize = CGSizeMake(0, CGRectGetHeight(self.view.bounds) * 2);
+        
+        [self.view addSubview:scrollView];
+    }
+    {
         UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
         [button setTitle:@"push" forState:UIControlStateNormal];
         [button addTarget:self action:@selector(pushAction) forControlEvents:UIControlEventTouchUpInside];
@@ -44,6 +56,7 @@
     {
         self.zl_navigationItem.rightBarButtonItems = @[[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(didSelectedRightBarButtonItem)],[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(didSelectedRightBarButtonItem)]];
     }
+
 //    {
 //        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), 100)];
 //        view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1];
